c5b6dee294604166f53b8db432aa7da01c10d372
cuitrying <53454868+cuitrying@users.noreply.github.com>
2025-04-05 18:45:03 +0300

    6.14, 6.15

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import { setTimedNotification } from './reducers/notificationReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    console.log('Fetching anecdotes...')
+    setLoading(true)
+    
+    dispatch(initializeAnecdotes())
+      .then(() => {
+        console.log('Anecdotes fetched successfully')
+        setLoading(false)
+      })
+      .catch(error => {
+        console.error('Error fetching anecdotes:', error)
+        dispatch(setTimedNotification('Error loading anecdotes. Is the server running?', 5))
+        setLoading(false)
+      })
+  }, [dispatch])
+  
   return (
     <div>
       <h2>Anecdotes</h2>
       <Notification />
       <Filter />
-      <AnecdoteList />
+      {loading ? <div>Loading anecdotes...</div> : <AnecdoteList />}
       <AnecdoteForm />
     </div>
   )
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,46 +1,69 @@
 import { createSlice } from '@reduxjs/toolkit'
-
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
-    voteAnecdote(state, action) {
-      const id = action.payload
-      const anecdoteToChange = state.find(a => a.id === id)
-      if (anecdoteToChange) {
-        anecdoteToChange.votes += 1
-      }
+    updateAnecdote(state, action) {
+      const updatedAnecdote = action.payload
+      return state.map(anecdote => 
+        anecdote.id !== updatedAnecdote.id ? anecdote : updatedAnecdote
+      )
     },
-    createAnecdote(state, action) {
-      state.push({
-        content: action.payload,
-        id: getId(),
-        votes: 0
-      })
+    appendAnecdote(state, action) {
+      state.push(action.payload)
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { voteAnecdote, createAnecdote } = anecdoteSlice.actions
+export const { updateAnecdote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    try {
+      console.log('Calling anecdoteService.getAll()...')
+      const anecdotes = await anecdoteService.getAll()
+      console.log('Received anecdotes:', anecdotes)
+      dispatch(setAnecdotes(anecdotes))
+      return Promise.resolve()
+    } catch (error) {
+      console.error('Error in initializeAnecdotes:', error)
+      return Promise.reject(error)
+    }
+  }
+}
+
+export const createAnecdote = (content) => {
+  return async dispatch => {
+    try {
+      const newAnecdote = await anecdoteService.createNew(content)
+      dispatch(appendAnecdote(newAnecdote))
+    } catch (error) {
+      console.error('Error creating anecdote:', error)
+    }
+  }
+}
+
+export const voteAnecdote = (id) => {
+  return async (dispatch, getState) => {
+    try {
+      const anecdotes = getState().anecdotes
+      const anecdoteToChange = anecdotes.find(a => a.id === id)
+      const changedAnecdote = { 
+        ...anecdoteToChange, 
+        votes: anecdoteToChange.votes + 1 
+      }
+      
+      const updatedAnecdote = await anecdoteService.update(id, changedAnecdote)
+      dispatch(updateAnecdote(updatedAnecdote))
+    } catch (error) {
+      console.error('Error voting for anecdote:', error)
+    }
+  }
+}
+
 export default anecdoteSlice.reducer
