2c5dd3382752e46cf9270283bfc33d8b2dc48a8c
cuitrying <53454868+cuitrying@users.noreply.github.com>
2025-04-05 19:18:47 +0300

    6.24

--- redux-anecdotes/src/requests.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const baseUrl = 'http://localhost:3001/anecdotes'
-
-export const getAnecdotes = async () => {
-  const response = await axios.get(baseUrl)
-  return response.data
-} 
